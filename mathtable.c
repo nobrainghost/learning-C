d2034f8d76159bb6871b1f911552606bd2731396
nobrainghost <mulwabenard24@gmaiil.com>
2024-04-02 19:47:04 +0300

    nothing

---
+++ mathtable.c
@@ -0,0 +1,73 @@
+////Multiplication table
+//#include "stdio.h"
+//int main(){
+//    int a,b,c,d,e,f,g,h,i,j;
+//    int cube_a;
+//    int cube_b;
+//    int cube_c;
+//    int cube_d;
+//    int cube_e;
+//    int cube_f;
+//    int cube_g;
+//    int cube_h;
+//    int cube_i;
+//    int cube_j;
+//    int sq_a;
+//    int sq_b;
+//    int sq_c;
+//    int sq_d;
+//    int sq_e;
+//    int sq_f;
+//    int sq_g;
+//    int sq_h;
+//    int sq_i;
+//    int sq_j;
+//
+//
+//
+//
+//    a=1;
+//    b=2;
+//    c=3;
+//    d=4;
+//    e=5;
+//    f=6;
+//    g=7;
+//    h=8;
+//    i=9;
+//    j=10;
+//    cube_a=b*a*a;
+//    cube_b=b*b*b;
+//    cube_c=c*c*c;
+//    cube_d=d*d*d;
+//    cube_e=e*e*e;
+//    cube_f=f*f*f;
+//    cube_g=g*g*g;
+//    cube_h=h*h*h;
+//    cube_i=i*i*i;
+//    cube_j=j*j*j;
+//    cube_i=i*i*i;
+//
+//    sq_a=a*a;
+//    sq_b=b*b;
+//    sq_c=c*c;
+//    sq_d=d*d;
+//    sq_e=e*e;
+//    sq_f=f*f;
+//    sq_g=g*g;
+//    sq_h=h*h;
+//    sq_i=i*i;
+//    sq_j=j*j;
+//    printf("number\t\tsquare\t\tcube\n");
+//    printf("%d\t\t\t%d\t\t\t%d\n",a,sq_a,cube_a);
+//    printf("%d\t\t\t%d\t\t\t%d\n",b,sq_b,cube_b);
+//    printf("%d\t\t\t%d\t\t\t%d\n",c,sq_c,cube_c);
+//    printf("%d\t\t\t%d\t\t\t%d\n",d,sq_d,cube_d);
+//    printf("%d\t\t\t%d\t\t\t%d\n",e,sq_e,cube_e);
+//    printf("%d\t\t\t%d\t\t\t%d\n",f,sq_f,cube_f);
+//    printf("%d\t\t\t%d\t\t\t%d\n",g,sq_g,cube_g);
+//    printf("%d\t\t\t%d\t\t\t%d\n",h,sq_h,cube_h);
+//    printf("%d\t\t\t%d\t\t\t%d\n",i,sq_i,cube_i);
+//    printf("%d\t\t\t%d\t\t\t%d\n",j,sq_j,cube_j);
+//
+//}
---
+++ max_min.c
@@ -0,0 +1,10 @@
+////Checks max from input integers
+//#include <stdio.h>
+//int main(){
+//    int a,b,c,d,e;
+//    printf("Please Input 5 integers:");
+//    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+//    if a>b
+//
+//    return 0;
+//}
