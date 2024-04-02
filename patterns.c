060fd54f0cf18de7305cb56ed144bb043bf7430d
nobrainghost <mulwabenard24@gmaiil.com>
2024-04-02 18:45:32 +0300

    nothing

---
+++ patterns.c
@@ -0,0 +1,12 @@
+/*This programs makes fun of patterns*/
+#include <stdio.h>
+int main(){
+    //Box
+    printf("This is a box\n");
+    printf("**********\n");
+    printf("*        *\n*        *\n*        *\n*        *\n*        *\n*        *\n**********\n");
+    //Oval
+    printf("***\n*   *\n*     *\n*       *\n*         *\n*         *\n*       *\n*     *\n*   *\n");
+    //Come on patterns are boring
+
+}
---
+++ product.c
@@ -0,0 +1,13 @@
+///*This is a product program that calculates of three user integers*/
+//#include <stdio.h>
+//int main(){
+//    int x;
+//    int y;
+//    int z;
+//    int result;
+//    printf("Please enter 3 integers:\n");
+//    scanf("%d%d%d", &x,&y,&z);
+//    result=x*y*z;
+//    printf("The product of %d and %d and %d is %d",x,y,z,result);
+//}
+//
