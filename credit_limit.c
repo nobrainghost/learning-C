d7a8f69a31ed83c3ad79a79ca8a7077bbaabfecd
nobrainghost <mulwabenard24@gmaiil.com>
2024-04-03 16:57:07 +0300

    missed an add

--- credit_limit.c
+++ credit_limit.c
@@ -1,3 +1,22 @@
-//
-// Created by benar on 4/3/24.
-//
+//#include "stdio.h"
+//int main(){
+//    int acct;
+//    int beginning_bal;
+//    int total;
+//    int limits;
+//    int new_bal;
+//    int credits;
+//    printf("Please enter credits applied:\n");
+//    scanf("%d",&credits);
+//    printf("Please enter your account number:\n");
+//    scanf("%d",&acct);
+//    printf("Please enter your beginning balance:\n");
+//    scanf("%d",&beginning_bal);
+//    printf("Please enter your total charges:\n");
+//    scanf("%d",&total);
+//    printf("Please enter your credit limit:\n");
+//    scanf("%d",&limits);
+//    new_bal=(beginning_bal+total)-credits;
+//    if(new_bal>=limits){ printf("Credit limit of %d exceeded as new balance is %d",limits,new_bal);}
+//    return 0;
+//}
