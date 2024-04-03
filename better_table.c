e12ed941456b00f51f80d8c3f109ca0195e7c87c
nobrainghost <mulwabenard24@gmaiil.com>
2024-04-03 22:29:31 +0300

    interest calc

--- better_table.c
+++ better_table.c
@@ -1,24 +1,24 @@
-#include "stdio.h"
-int main(){
-    int counter;
-    int last_number;
-    int cube;
-    int square;
-    int hundreds;
-    counter=0;
-    printf("Please input the last number you wish:\n");
-    scanf("%d",&last_number);
-    printf("Number\t\t\tSquare\t\t\tCube\t\t\tHundreds\n");
-
-    while (counter<=last_number){
-        square=counter*counter;
-        cube=counter*counter*counter;
-        hundreds=counter*100;
-        counter++;
-
-        printf("%d\t\t\t\t%d\t\t\t\t%d\t\t\t\t%d\n",counter,square,cube,hundreds);
-
-    }
-
-}
-//Memory***
+//#include "stdio.h"
+//int main(){
+//    int counter;
+//    int last_number;
+//    int cube;
+//    int square;
+//    int hundreds;
+//    counter=0;
+//    printf("Please input the last number you wish:\n");
+//    scanf("%d",&last_number);
+//    printf("Number\t\t\tSquare\t\t\tCube\t\t\tHundreds\n");
+//
+//    while (counter<=last_number){
+//        square=counter*counter;
+//        cube=counter*counter*counter;
+//        hundreds=counter*100;
+//        counter++;
+//
+//        printf("%d\t\t\t\t%d\t\t\t\t%d\t\t\t\t%d\n",counter,square,cube,hundreds);
+//
+//    }
+//
+//}
+////Memory***
---
+++ interest.c
@@ -0,0 +1,13 @@
+//#include <stdio.h>
+//#include <math.h>
+//int main(){
+//    int year;
+//    float rate=.05;
+//    float amount;
+//    float principal=1000.00;
+//    printf("Year\t\t\t\tAmount on Deposit\n");
+//    for(year=1;year<=10;year++){
+//        amount=principal*pow(1.0+rate,year);
+//    printf("%4d\t\t\t\t%.2f\n",year,amount);}
+//
+//}
