2efbe0a20c7db2ec555d6f299788b39d05c65e2f
nobrainghost <mulwabenard24@gmaiil.com>
2024-04-03 19:18:25 +0300

    More control

---
+++ milespergallon.c
@@ -0,0 +1,13 @@
+//#include "stdio.h"
+//int main() {
+//    int miles;
+//    int gallons;
+//    float miles_per_gallon;
+//    printf("Please Enter Gallons you refuelled:\n");
+//    scanf("%d", &gallons);
+//    printf("Now enter Kilometres Driven:\n");
+//    scanf("%d", &miles);
+//    miles_per_gallon = (float) miles / (float) gallons;
+//    printf("Today the consumption rate is %.2f miles per gallon",miles_per_gallon);
+//    return 0;
+//}
