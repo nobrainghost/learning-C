9fd32618dd5500308022641cc2102fa7d4133491
nobrainghost <mulwabenard24@gmaiil.com>
2024-04-04 15:01:43 +0300

    grade counter

---
+++ grade_counter.c
@@ -0,0 +1,64 @@
+////
+//// Created by benar on 4/4/24.
+////
+//#include "stdio.h"
+//int main(){
+//
+//    int grade;
+//    int count_a=0;
+//    int count_b=0;
+//    int count_c=0;
+//    int count_d=0;
+//    int count_e=0;
+//    int count_f=0;
+//    char q;
+//    while((grade=getchar()) !='q') {
+//        switch (grade) {
+//            case 'a':
+//            case 'A':
+//                count_a++;
+//                break;
+//            case 'b':
+//            case 'B':
+//                count_b++;
+//                break;
+//            case 'c':
+//            case 'C':
+//                count_c++;
+//                break;
+//            case 'd':
+//            case 'D':
+//                count_d++;
+//                break;
+//
+//            case 'e':
+//            case 'E':
+//                count_e++;
+//                break;
+//
+//
+//            case 'f':
+//            case 'F':
+//                count_f++;
+//                break;
+//
+//            case '\n':
+//            case '\t':
+//            case ' ':
+//                break;
+//
+//            default:
+//                printf("Invalid Character\n");
+//                break;
+//
+//
+//        }
+//    }
+//    printf("Output Summary\n");
+//    printf("A's are: %d\n",count_a);
+//    printf("B's are: %d\n",count_b);
+//    printf("C's are: %d\n",count_c);
+//    printf("D's are: %d\n",count_d);
+//    printf("E's are: %d\n",count_e);
+//    printf("F's are: %d\n",count_f);
+//}
